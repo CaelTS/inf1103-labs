1bf0d580ab5837ae7d9f1c61d2c22a514d88c655
Cael Thierry Saleh <2502622@sit.singaporetech.edu.sg>
2026-09-11 11:43:27 +0800

    Reporting

--- auditor.py
+++ auditor.py
@@ -1,4 +1,5 @@
-inventory = 0
+inventory = 0;
+fr_entries = 0;
 
 while True:
     stock = input("\nEnter Stock Quantity: ")
@@ -7,9 +8,12 @@ while True:
         print(inventory)
     elif stock == "quit":
         print("\nTotal Units Processed: ", inventory);
+        print("Number of Failed/Rejected Entries:", fr_entries);
+        print("\n")
         break;
     else:
-        print("Not a valid number\n")
+        print("Not a number\n")
+        fr_entries = fr_entries + 1;
 
     if inventory >= 500:
         print("\nInventory exceeds 500 quantity\n")
